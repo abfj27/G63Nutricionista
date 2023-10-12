77bd78bd10c350beb9d6502bd09104ba022b1757
KevinPaico <141683583+KevinPaico@users.noreply.github.com>
2023-10-12 20:20:18 -0300

    Update Test.java

--- src/g63nutricionista/Test.java
+++ src/g63nutricionista/Test.java
@@ -18,64 +18,64 @@ public class Test {
 
     public static void main(String[] args) {
 
-        PacienteData pd = new PacienteData();
-        Paciente pac1 = new Paciente(1231, "Bavastro", "Alesio", 60, 1.70, 30, "M",  "Avenida Siempre Viva", "+54 1231", "asd@hotmail.com", 1);
-        Paciente pac2 = new Paciente(1232, "Kaminski", "Nicolas",75, 1.80, 33, "M", "Avenida Siempre Viva", "+54 1232", "asd@hotmail.com", 1);
-        Paciente pac3 = new Paciente(1233, "Perez", "Maria", 80, 2, 22, "F", "Avenida Siempre Viva", "+54 1233", "asd@hotmail.com", 1);
-        ComidaData cd = new ComidaData();
-        Comida com1 = new Comida("Pollo", "Pollo al horno", 300, 1);
-        Comida com2 = new Comida("Arroz", "Arroz al vapor", 250, 1);
-        Comida com3 = new Comida("Huevo", "Huevo frito", 50, 1);
-        DietaData dd = new DietaData();
-        Dieta die1 = new Dieta("Subir peso paciente007", pac1, 60, 80, LocalDate.now(), LocalDate.now(), 1);
-        Dieta die2 = new Dieta("Subir peso paciente008", pac2, 100, 80, LocalDate.now(), LocalDate.now(), 1);
-        Dieta die3 = new Dieta("Bajar peso paciente009", pac3, 120, 90, LocalDate.now(), LocalDate.now(), 1);
-        VisitaData vd = new VisitaData();
-        Visita vis1 = new Visita(die1, pac1, 60, LocalDate.now(), 1);
-        Visita vis2 = new Visita(die1, pac1, 63, LocalDate.now(), 1);
-        Visita vis3 = new Visita(die1, pac1, 62, LocalDate.now(), 1);
-        DietaComidaData dc = new DietaComidaData();
-        DietaComida dc01 = new DietaComida(com1, die1, Horario.ALMUERZO, Porcion.MEDIANA, 1);
-        DietaComida dc02 = new DietaComida(com2, die2, Horario.CENA, Porcion.MEDIANA, 1);
-        DietaComida dc03 = new DietaComida(com3, die1, Horario.DESAYUNO, Porcion.MEDIANA, 1);
-//
-        pd.cargarPaciente(pac1);
-        pd.cargarPaciente(pac2);
-        pd.cargarPaciente(pac3);
-        cd.cargarComida(com1);
-        cd.cargarComida(com2);
-        cd.cargarComida(com3);
-        dd.cargarDieta(die1);
-        dd.cargarDieta(die2);
-        dd.cargarDieta(die3);
-        vd.cargarVisita(vis1);
-        vd.cargarVisita(vis2);
-        vd.cargarVisita(vis3);
-        dc.cargarDietaComida(dc01);
-        dc.cargarDietaComida(dc02);
-        dc.cargarDietaComida(dc03);
-//
-        pac2.setDomicilio("Avenida Siempre Viva 69");
-        pac2.setEmail("asd@hotmail.com.nico");
-        pd.modificarPaciente(pac2);
-        pd.eliminarPaciente(pac3);
-//
-        com2.setCalorias(245);
-        cd.modificarComida(com2);
-        cd.eliminarComida(com3);
-//
-        die2.setPesoInicial(66.6);
-        die2.setPesoObjetivo(88.8);
-        dd.modificarDieta(die2);
-        dd.eliminarDieta(die3);
-//
-        vis2.setPeso(61.5);
-        vd.modificarVisita(vis2);
-        vd.eliminarVisita(vis3);
-//        
-        dc02.setHorario(Horario.ALMUERZO);
-        dc.modificarDietaComida(dc02);
-        dc.eliminarDietaComida(dc03);
+//        PacienteData pd = new PacienteData();
+//        Paciente pac1 = new Paciente(1231, "Bavastro", "Alesio", 60, 1.70, 30, "M",  "Avenida Siempre Viva", "+54 1231", "asd@hotmail.com", 1);
+//        Paciente pac2 = new Paciente(1232, "Kaminski", "Nicolas",75, 1.80, 33, "M", "Avenida Siempre Viva", "+54 1232", "asd@hotmail.com", 1);
+//        Paciente pac3 = new Paciente(1233, "Perez", "Maria", 80, 2, 22, "F", "Avenida Siempre Viva", "+54 1233", "asd@hotmail.com", 1);
+//        ComidaData cd = new ComidaData();
+//        Comida com1 = new Comida("Pollo", "Pollo al horno", 300, 1);
+//        Comida com2 = new Comida("Arroz", "Arroz al vapor", 250, 1);
+//        Comida com3 = new Comida("Huevo", "Huevo frito", 50, 1);
+//        DietaData dd = new DietaData();
+//        Dieta die1 = new Dieta("Subir peso paciente007", pac1, 60, 80, LocalDate.now(), LocalDate.now(), 1);
+//        Dieta die2 = new Dieta("Subir peso paciente008", pac2, 100, 80, LocalDate.now(), LocalDate.now(), 1);
+//        Dieta die3 = new Dieta("Bajar peso paciente009", pac3, 120, 90, LocalDate.now(), LocalDate.now(), 1);
+//        VisitaData vd = new VisitaData();
+//        Visita vis1 = new Visita(die1, pac1, 60, LocalDate.now(), 1);
+//        Visita vis2 = new Visita(die1, pac1, 63, LocalDate.now(), 1);
+//        Visita vis3 = new Visita(die1, pac1, 62, LocalDate.now(), 1);
+//        DietaComidaData dc = new DietaComidaData();
+//        DietaComida dc01 = new DietaComida(com1, die1, Horario.ALMUERZO, Porcion.MEDIANA, 1);
+//        DietaComida dc02 = new DietaComida(com2, die2, Horario.CENA, Porcion.MEDIANA, 1);
+//        DietaComida dc03 = new DietaComida(com3, die1, Horario.DESAYUNO, Porcion.MEDIANA, 1);
+////
+//        pd.cargarPaciente(pac1);
+//        pd.cargarPaciente(pac2);
+//        pd.cargarPaciente(pac3);
+//        cd.cargarComida(com1);
+//        cd.cargarComida(com2);
+//        cd.cargarComida(com3);
+//        dd.cargarDieta(die1);
+//        dd.cargarDieta(die2);
+//        dd.cargarDieta(die3);
+//        vd.cargarVisita(vis1);
+//        vd.cargarVisita(vis2);
+//        vd.cargarVisita(vis3);
+//        dc.cargarDietaComida(dc01);
+//        dc.cargarDietaComida(dc02);
+//        dc.cargarDietaComida(dc03);
+////
+//        pac2.setDomicilio("Avenida Siempre Viva 69");
+//        pac2.setEmail("asd@hotmail.com.nico");
+//        pd.modificarPaciente(pac2);
+//        pd.eliminarPaciente(pac3);
+////
+//        com2.setCalorias(245);
+//        cd.modificarComida(com2);
+//        cd.eliminarComida(com3);
+////
+//        die2.setPesoInicial(66.6);
+//        die2.setPesoObjetivo(88.8);
+//        dd.modificarDieta(die2);
+//        dd.eliminarDieta(die3);
+////
+//        vis2.setPeso(61.5);
+//        vd.modificarVisita(vis2);
+//        vd.eliminarVisita(vis3);
+////        
+//        dc02.setHorario(Horario.ALMUERZO);
+//        dc.modificarDietaComida(dc02);
+//        dc.eliminarDietaComida(dc03);
   }
 
 }
